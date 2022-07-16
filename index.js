dfe39b3012ecb79792292f2eda87cbd1f6c26bf5
abhishekjinxed <abhishekjinxed@gmail.com>
2022-07-16 20:54:33 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 import express from 'express';
-import mongoose from 'mongoose';
-
 const app = express();
+import mongoose from 'mongoose';
+const port = 8080;
 
 const url =
   'mongodb+srv://rickc137:Asaprocky92@tiktokbackend.jgdtf7j.mongodb.net/?retryWrites=true&w=majority';
-mongoose.connect(url, { useNewUrlParser: true }).then(() => {
-  app.listen(5000, () => {
-    console.log('Server has started!');
-  });
+mongoose.connect(
+  url,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) {
+      console.log('error in connection');
+    } else {
+      console.log('mongodb is connected');
+    }
+  }
+);
+//create a server object:
+app.listen(port, () => {
+  console.log('starting the server');
 });
