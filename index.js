c5594de9b1df618374ddf0cdbadbd0cfe2012751
abhishekjinxed <abhishekjinxed@gmail.com>
2022-07-17 15:58:49 +0530

    feafa

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+import * as dotenv from 'dotenv'
 import express from 'express';
 const app = express();
 import mongoose from 'mongoose';
 const port = 8080;
-require('dotenv').config()
 
+
+dotenv.config()
 
 mongoose.connect(
-  process.env.DB_HOST,
+  process.env.MONGO_URL,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
